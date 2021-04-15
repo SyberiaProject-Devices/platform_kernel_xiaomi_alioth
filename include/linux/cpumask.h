d268f75b62dffd3e01f2caa17b6fa4ef5e96ca30
Adam W. Willis <return.of.octobot@gmail.com>
2021-04-15 19:04:25 -0400

    cpumask: Refactor prime cluster cpumask
    Bring inline with 30c2f76 ("kernel: Add tri-cluster API to
    affine IRQs and kthreads to fast CPUs")
    
    [@0ctobot: cpu_input_boost: Update cpu_perfp_mask usage]
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- include/linux/cpumask.h
+++ include/linux/cpumask.h
@@ -58,7 +58,7 @@ extern unsigned int nr_cpu_ids;
  *     cpu_isolated_mask- has bit 'cpu' set iff cpu isolated
  *     cpu_lp_mask      - has bit 'cpu' set iff cpu is part of little cluster
  *     cpu_perf_mask    - has bit 'cpu' set iff cpu is part of big cluster
- *     cpu_perfp_mask   - has bit 'cpu' set iff cpu is part of prime cluster
+ *     cpu_prime_mask   - has bit 'cpu' set iff cpu is part of prime cluster
  *
  *  If !CONFIG_HOTPLUG_CPU, present == possible, and active == online.
  *
@@ -96,8 +96,6 @@ extern struct cpumask __cpu_online_mask;
 extern struct cpumask __cpu_present_mask;
 extern struct cpumask __cpu_active_mask;
 extern struct cpumask __cpu_isolated_mask;
-extern struct cpumask __cpu_lp_mask;
-extern struct cpumask __cpu_perf_mask;
 #define cpu_possible_mask ((const struct cpumask *)&__cpu_possible_mask)
 #define cpu_online_mask   ((const struct cpumask *)&__cpu_online_mask)
 #define cpu_present_mask  ((const struct cpumask *)&__cpu_present_mask)
@@ -105,7 +103,7 @@ extern struct cpumask __cpu_perf_mask;
 #define cpu_isolated_mask ((const struct cpumask *)&__cpu_isolated_mask)
 extern const struct cpumask *const cpu_lp_mask;
 extern const struct cpumask *const cpu_perf_mask;
-extern const struct cpumask *const cpu_perfp_mask;
+extern const struct cpumask *const cpu_prime_mask;
 
 extern atomic_t __num_online_cpus;
 
