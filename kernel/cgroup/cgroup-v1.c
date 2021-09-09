7ee1f7f7b174c068d651ce1fb90dbdc1e9447b78
Emanuel <emanuelghub@gmail.com>
2021-09-09 17:14:08 +0400

    fs: exec: Swich to task_is_zygote

--- kernel/cgroup/cgroup-v1.c
+++ kernel/cgroup/cgroup-v1.c
@@ -541,11 +541,13 @@ static ssize_t __cgroup1_procs_write(struct kernfs_open_file *of,
 	ret = cgroup_attach_task(cgrp, task, threadgroup);
 
 	/* This covers boosting for app launches and app transitions */
-	if (!ret && !threadgroup &&
-		!memcmp(of->kn->parent->name, "top-app", sizeof("top-app")) &&
-		is_zygote_pid(task->parent->pid)) {
-		cpu_input_boost_kick_max(1000);
-	}	
+        if (!ret && !threadgroup &&
+               !memcmp(of->kn->parent->name, "top-app", sizeof("top-app")) &&
+               task_is_zygote(tsk->parent)) {
+                devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 500);
+                devfreq_boost_kick_max(DEVFREQ_MSM_LLCCBW, 500);
+
+        }
 
 out_finish:
 	cgroup_procs_write_finish(task);
